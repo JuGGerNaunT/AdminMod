8c1742b67e39e99024771121dd3427b8f0a20c56
JuGGerNaunT <hrono8desmond15@yandex.ru>
2013-01-17 02:22:03 +0300

    new breath - entire project

--- MyPlugin.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-extern int players_id[32];
-
-void PluginInit();
-void AdminInfo();
-void ShowList();
-void SndMessage();
-void KickPlayer();
--- testing.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <extdll.h>
-
-qboolean ClientConnect(edict_t *pEntity, const char *pszName, const char *pszAddress, char szRejectReason[ 128 ]);
